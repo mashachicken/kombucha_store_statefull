a3a9320bc9cf7a98e836302f0735797e95b7a91b
Masha Ivy <mashanechmo@gmail.com>
2020-02-14 12:58:11 -0800

    add a kombuchas componennt and route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import  from './';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <header className="App-header">
+      <Nav />
+    </header>
+      <Switch>
+        <Route exact path='/' component={} />
+        <Route path='/all-products' component={Kombuchas} />
+    </Switch>
     </div>
   );
 }
